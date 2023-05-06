e9fb8ad620d8e1191d5d7d95d258bee07a9782d7
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2023-05-05 23:23:48 -0300

    Estudos 05/05/23

--- Seção11_JavaScript_Node_ProjetoAgenda/projetoagenda/src/middlewares/middleware.js
+++ Seção11_JavaScript_Node_ProjetoAgenda/projetoagenda/src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middleWareGlobal = (req,res,next) => {
     res.locals.errors = req.flash('errors');
     res.locals.sucess = req.flash('sucess');
+    res.locals.user = req.session.user; 
     next();
 };
 
