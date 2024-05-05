bbbb32507b9ccbdf6c541b6e8779049deec778bf
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2024-05-05 12:32:34 -0300

    add: react route dom v6

---
+++ Seção13_React_basico_JS/tutorial-react-route-dom/src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+    return (
+    <div>
+        <h1>Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ullam cum voluptate officiis, saepe nihil sapiente quibusdam. Modi asperiores quo, reprehenderit atque quod aliquam similique voluptates cupiditate obcaecati, aliquid mollitia.</p>
+    </div>
+    );
+};
